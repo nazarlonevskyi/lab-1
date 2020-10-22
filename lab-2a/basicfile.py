63cdd7de2595e9c1b784a858a1313d79a178c860
nazarlonevskyi <nazar.lonevskyi.itis.2018@lpnu.ua>
2020-10-22 05:52:47 -0400

    Change file .py

---
+++ lab-2a/basicfile.py
@@ -0,0 +1,29 @@
+print("First constant", False)
+print("Second constant", True)
+print("None constant", None)
+
+print(abs(-10.11), f" {abs(10.11)}")
+print(bin(1234))
+print(float('+5.67'))
+
+B = True
+print("So B=True" if B else " So B=False")
+B = False
+print("So B=True" if B else " So B=False")
+
+try:
+    a = int(input("the first number: "))
+    b = int(input("the second number: "))
+    print (a / b)
+except (ValueError, ZeroDivisionError):
+    print("WTF?...")
+finally:
+    print("THE END).")
+
+with open('README.md', 'w') as opened_file:
+    opened_file.write('Hello!')
+
+full_name = lambda first, last: f'Full name{first} {last}'
+full_name('Nazar','Lonevskyi')
+print("Simple function", full_name)
+print("Call function", full_name ('Nazar', 'Lonevskyi'))
